a2a815718ae2f381959ed3e6b1712f520f111301
Alexandra Robin <ra401651@etu.unice.fr>
2017-12-19 09:25:02 +0100

    MAJ Version Alex

--- VersionAlex/script/Obstacle.js
+++ VersionAlex/script/Obstacle.js
@@ -1,38 +1,32 @@
 class Obstacle extends Objet {
 	constructor(x,vx,vy,w,h,vie,img) {
 		super(x,-h,vx,vy,w,h,vie,img);
-		// x : défini dans création
-		// y : -h par défaut
-		// vx,vy,w,h,vie, image : défini dans fils
+	}
+
+	moveX() { this.posX += this.vitesseX; }
+	moveY() { this.posY += this.vitesseY; }
+
+	animer() {
+		this.moveX();
+		this.moveY();
 	}
 
 	out(ctx) {
 		var w = ctx.canvas.width;
 		var h = ctx.canvas.height;
-		return (this.posY >= h) || (this.posX+this.largeur <= 0) || (this.posX >= w);
+		return (this.posY >= h) || (this.posX + this.largeur <= 0) || (this.posX >= w); // bas|gauche|droite
 	}
 }
-
-// dans classe suivantes, dessiner() est temporaire, il est défini dans la classe Objet
 
 class ObstacleEasy extends Obstacle {
 	constructor(x) {
 		let vx=0;
 		let vy=1;
-		let w=10;
-		let h=10;
+		let w=30;
+		let h=30;
 		let vie=0;
-		let img=0;
+		let img = document.querySelector("#meteor");
 		super(x,vx,vy,w,h,vie,img);
-		//super(x,0,1,10,10,0,0);
-	}
-	dessiner(ctx) {
-		ctx.save();
-		ctx.translate(this.posX, this.posY);
-		ctx.fillStyle = "green";
-		ctx.fillRect(0,0,this.largeur,this.hauteur);
-		//ctx.translate(-this.posX, -this.posY);
-		ctx.restore();
 	}
 }
 
@@ -40,20 +34,11 @@ class ObstacleMedium extends Obstacle {
 	constructor(x) {
 		let vx=0;
 		let vy=2;
-		let w=20;
-		let h=20;
+		let w=50;
+		let h=50;
 		let vie=10;
-		let img=0;
+		let img = document.querySelector("#asteroid");
 		super(x,vx,vy,w,h,vie,img);
-		//super(x,0,2,20,20,10,0);
-	}
-	dessiner(ctx) {
-		ctx.save();
-		ctx.translate(this.posX, this.posY);
-		ctx.fillStyle = "orange";
-		ctx.fillRect(0,0,this.largeur,this.hauteur);
-		//ctx.translate(-this.posX, -this.posY);
-		ctx.restore();
 	}
 }
 
@@ -61,19 +46,10 @@ class ObstacleHard extends Obstacle {
 	constructor(x) {
 		let vx=0;	// random vx ?
 		let vy=3;
-		let w=20;
-		let h=20;
+		let w=70;
+		let h=70;
 		let vie=20;
-		let img=0;
+		let img = document.querySelector("#milky");
 		super(x,vx,vy,w,h,vie,img);
-		//super(x,0,3,20,20,20,0);
-	}
-	dessiner(ctx) {
-		ctx.save();
-		ctx.translate(this.posX, this.posY);
-		ctx.fillStyle = "red";
-		ctx.fillRect(0,0,this.largeur,this.hauteur);
-		//ctx.translate(-this.posX, -this.posY);
-		ctx.restore();
 	}
 }
